0f1cebb3b09522de61324ac9d23862fa8bcdedc9
Ahmed <ahmedmzaki22@gmail.com>
2017-11-15 17:51:15 +0200

    using compenent task-list <task-list> </task-list>

--- task.js
+++ task.js
@@ -1,10 +1,19 @@
-Vue.component('task',{
-template:'<li><slot></slot></li>',
+Vue.component('task-list',{
+template:'<div><task v-for="task in tasks">{{task.task}}</task></div>',
 data(){
   return {
-    message:'foobar'
+    tasks:[
+      {task:'go to store ',completed:true},
+      {task:'go to school ',completed:true},
+      {task:'go to street ',completed:true},
+      {task:'go to heaven ',completed:true}
+    ]
   };
 }
+});
+
+Vue.component('task',{
+  template:'<li><slot></slot></li>'
 });
 
 new Vue({
